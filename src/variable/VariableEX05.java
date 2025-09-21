3aca4df11b4641943afae16577315777eff2322d
Blue <isyuyong6732@naver.com>
2025-09-21 13:12:52 +0900

    공부 + 숙제중!

--- src/variable/VariableEX05.java
+++ src/variable/VariableEX05.java
@@ -13,7 +13,17 @@ public class VariableEX05 {
         //  9  18  27  36  45  54  63  72  81
         int[][] gugudan = new int[8][9];
 
-
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 9; j++) {
+                gugudan[i][j] = (i+2) * (j+1);
+            }
+        }
+        for (int[] dan : gugudan) {
+            for (int gugu : dan) {
+                System.out.print(String.format("%4d", gugu));
+            }
+            System.out.println();
+        }
     }
 
     public static void ex02() {
@@ -35,9 +45,16 @@ public class VariableEX05 {
                 {3, 5},  // 401호, 402호
                 {4, 4}   // 501호, 502호
         };
-
-
-
+        // 구구처럼 포문인가?
+        for (int i = 0; i < apt.length; i++) {
+            // apt.length는 5임 5개니까 배열이긴해도
+            for (int j = 0; j < apt[i].length; j++) {
+                // 이건 2개씩이겠다 apt[0]이랑 apt[1]이 되니까
+                int hoNumber = (i + 1) * 100 + (j + 1); // 호넘버는 = 101호 / 102호, 그담 201호 202호
+                System.out.printf("%d호: %d명\n", hoNumber, apt[i][j]);
+                // honumber는 실제 호고 apt[i][j]는 반복문에서 나온숫자로 실제 몇명인지 출력
+            }
+        }
     }
 
     public static void ex03() {
@@ -54,9 +71,17 @@ public class VariableEX05 {
                 {3, 5},  // 4층
                 {4, 4}   // 5층
         };
-
-
-
+        // 포문 만는듯
+        for (int i = 0; i < apt.length; i++) {
+            int peoplesu = 0;
+            // apt.length는 똑같이 5임 i는 층이군
+            for (int j = 0; j < apt[i].length; j++) {
+                // 이것도 똑같이 2개씩이겠다 apt[0]이랑 apt[1]이 되니까
+                peoplesu = peoplesu + apt[i][j];
+                // honumber는 실제 호고 apt[i][j]는 반복문에서 나온숫자로 실제 몇명인지 출력
+            }
+            System.out.printf("%d층: %d명\n", i+1 , peoplesu);
+        }
     }
 
     public static void ex04() {
@@ -74,9 +99,15 @@ public class VariableEX05 {
                 {"음악", "국어", "윤리"},
                 {"수학", "영어", "체육", "과학"}
         };
-
-
-
+        System.out.println("시간표 출력하기");
+        for (int i = 0; i < weekname.length; i++) {
+            System.out.print(weekname[i] + ":");
+            for (int j = 0; j < timeTable[i].length; j++) {
+                System.out.print(" " + timeTable[i][j] + " ");
+            }
+            System.out.println();
+            // 끼얗호응~
+        }
     }
 
     public static void ex05() {
@@ -100,16 +131,16 @@ public class VariableEX05 {
                 {0, 0, 1, 0, 0}
         };
         int[][] b = new int[5][5];
-
-
     }
+    //  네????????????????????????????????????ㅋㅋㅋㅋㅋㅋㅋㅋ
+    // ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+
     public static void main(String[] args) {
 //  ex01();
 //  ex02();
 //  ex03();
-//  ex04();
+  ex04();
 //  ex05();
 
     }
-
 }
