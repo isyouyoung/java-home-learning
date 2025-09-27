071360e81d816c142ded69dbde8d1e9e858e90af
Blue <isyuyong6732@naver.com>
2025-09-28 01:56:13 +0900

    !!!!!!!!공부 진행중!!!!!!! ex03 진행중

---
+++ src/variable/ex08/ex03_Soldier/MainWrapper.java
@@ -0,0 +1,20 @@
+package variable.ex08.ex03_Soldier;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+
+   Soldier s = new Soldier(30);
+   // s = Solier  클래스로 만든 객체임 근대 매개변수로 bullet 30을 줌
+
+   s.reload(30); //ㅇㅋ
+   s.reload(20); //ㅇㅋ
+   
+   for(int n = 0; n < 50; n++) {
+     s.shoot(); //50발 쏨
+   }
+   s.shoot();
+   s.shoot();
+  }
+
+}
