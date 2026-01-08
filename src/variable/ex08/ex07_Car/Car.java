658f1b678a27b1104dc319658d71cab9ecff7476
BLUE <isyouyoung@gmail.com>
2026-01-08 23:14:08 +0900

    공부

--- src/variable/ex08/ex07_Car/Car.java
+++ src/variable/ex08/ex07_Car/Car.java
@@ -1,59 +1,65 @@
 package variable.ex08.ex07_Car;
 
 public class Car {
+
   private Driver driver;
-  private int speed;
   private int fuel;
+  private int speed;
   private final int MAX_SPEED = 100;
+
   public void setDriver(Driver driver) {
     this.driver = driver;
   }
-  public Driver getDriver() {
-    return driver;
-  }
-  public void setSpeed(int speed) {
-    this.speed = speed;
-  }
-  public int getSpeeed() {
-    return speed;
-  }
+
   public void setFuel(int fuel) {
     this.fuel = fuel;
   }
-  public int getFuel() {
-    return fuel;
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
   }
-  
+
+  public Driver getDriver() {
+    return this.driver;
+  }
+
+  public int getFuel() {
+    return this.fuel;
+  }
+
+  public int getSpeed() {
+    return this.speed;
+  }
+
   public void engineStart() {
-    if(fuel == 0) {
-      System.out.println("시동이 걸리지 않았습니다.");
+    if (this.fuel > 0) {
+      System.out.println("시동이 걸렸습니다.");
     } else {
-        System.out.println("시동이 걸렸습니다.");
+      System.out.println("시동이 걸리지 않았습니다.");
     }
   }
-  
-  
+
   public void drive() {
-    if(fuel == 0 || driver == null) {
+    if (this.speed == 0) {
       System.out.println("자동차가 움직이지 않았습니다.");
-    }else {
+    } else {
       System.out.println("자동차가 움직였습니다.");
     }
   }
-  
+
   public void accel(int speed) {
-    for(int n = 0; n < speed; n++) {
-      if(this.speed == MAX_SPEED || fuel == 0) {
+    for (int i = 0; i < speed; i++ ) {
+      if (this.speed == MAX_SPEED || fuel == 0) {
         return;
       }
       this.speed++;
-      fuel--;
+      this.fuel--;
     }
-    
   }
+
   public void brake(int speed) {
-    for(int n = 0; n < speed; n++) {
-      if(this.speed == 0) {
+    for (int i = 0; i < speed; i++ ) {
+      if (this.speed == 0) {
         System.out.println("현재 속도는 " + this.speed + "입니다.");
         return;
       }
@@ -61,11 +67,5 @@ public class Car {
     }
     System.out.println("현재 속도는 " + this.speed + "입니다.");
   }
-  
-  
-  
-  // boolean 타입의 getter setter이름에 is가 있으면 삭제한다.
- 
-  
-  
+
 }
