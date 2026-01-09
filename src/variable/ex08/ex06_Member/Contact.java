1aa07e8cf2ca8f9aa28b50ff4f9e6533e52e9154
BLUE <isyouyoung@gmail.com>
2026-01-09 22:09:26 +0900

    공부

--- src/variable/ex08/ex06_Member/Contact.java
+++ src/variable/ex08/ex06_Member/Contact.java
@@ -1,28 +1,33 @@
 package variable.ex08.ex06_Member;
 
 public class Contact {
+
   private String homeTel;
-  private String moblie;
+  private String mobile;
   private Address address;
-  
+
   public void setHomeTel(String homeTel) {
     this.homeTel = homeTel;
   }
+
   public void setMobile(String mobile) {
-    this.moblie = mobile;
+    this.mobile = mobile;
   }
+
   public void setAddress(Address address) {
     this.address = address;
   }
-  
+
   public String getHomeTel() {
-    return homeTel;
+    return this.homeTel;
   }
-  public String getMoblie() {
-    return moblie;
+
+  public String getMobile() {
+    return this.mobile;
   }
+
   public Address getAddress() {
-    return address;
+    return this.address;
   }
-  
+
 }
