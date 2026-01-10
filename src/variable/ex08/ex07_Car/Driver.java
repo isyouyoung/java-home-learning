5bc83880bb06cab05ccc2887b6bc157599ba97cc
BLUE <isyouyoung@gmail.com>
2026-01-10 22:44:11 +0900

    공부

--- src/variable/ex08/ex07_Car/Driver.java
+++ src/variable/ex08/ex07_Car/Driver.java
@@ -1,30 +1,24 @@
 package variable.ex08.ex07_Car;
 
 public class Driver {
+
   private String name;
   private int career;
-  private boolean bestDriver;     // career >= 10 ture
-  
+
   public void setName(String name) {
     this.name = name;
   }
-  public String getName() {
-    return name;
-  }
+
   public void setCareer(int career) {
     this.career = career;
-    setBestDriver(career >= 10);
   }
+
+  public String getName() {
+    return this.name;
+  }
+
   public int getCareer() {
-    return career;
-  }
-
-  private void setBestDriver(boolean bestDriver) {
-    this.bestDriver = bestDriver;
-  }
-
-  public boolean isBestDriver() {       // boolean의 getter이름은 get대신에 is로 대체한다.
-    return bestDriver;
+    return this.career;
   }
 
 }
