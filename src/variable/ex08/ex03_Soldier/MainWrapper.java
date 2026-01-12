e196f6e5c9cfc10577a27b3a025730e817e6f1ad
BLUE <isyouyoung@gmail.com>
2026-01-12 19:08:06 +0900

    공부

--- src/variable/ex08/ex03_Soldier/MainWrapper.java
+++ src/variable/ex08/ex03_Soldier/MainWrapper.java
@@ -5,17 +5,14 @@ public class MainWrapper {
   public static void main(String[] args) {
 
    Soldier s = new Soldier(30);
-   // s = Solier  클래스로 만든 객체임 근대 매개변수로 bullet 30을 줌
 
-   s.reload(30); //ㅇㅋ
-   s.reload(20); //ㅇㅋ
-   
+   s.reload(30);
+   s.reload(20);
+
    for(int n = 0; n < 50; n++) {
-     s.shoot(); //50발 쏨
+     s.shoot();
    }
    s.shoot();
    s.shoot();
   }
-  // 잔디 이제되네 2
-
 }
