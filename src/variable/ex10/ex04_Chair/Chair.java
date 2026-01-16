4c5ac462af70d1133ccb860e0ea0d5568483eb3b
BLUE <isyouyoung@gmail.com>
2026-01-16 21:21:35 +0900

    공부

---
+++ src/variable/ex10/ex04_Chair/Chair.java
@@ -0,0 +1,15 @@
+package variable.ex10.ex04_Chair;
+
+public class Chair {
+
+  private Person person;
+
+  public void setPerson(Person person) {
+    this.person = person;
+  }
+
+  public Person getPerson() {
+    return this.person;
+  }
+
+}
