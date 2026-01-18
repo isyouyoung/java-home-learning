67297f331baa0c4d9de7f9a2fc3ab82a65601cc8
BLUE <isyouyoung@gmail.com>
2026-01-18 21:49:32 +0900

    공부

--- src/variable/ex10/ex02_Computer/Computer.java
+++ src/variable/ex10/ex02_Computer/Computer.java
@@ -2,18 +2,17 @@ package variable.ex10.ex02_Computer;
 
 public class Computer {
 
-    // 필드
     private String model;
 
-    // 생성자
-    public Computer(){
-    }
-    public Computer(String model){
+    public Computer(String model) {
         this.model = model;
     }
 
-    // 메서드
-    public String getModel(){
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
         return this.model;
     }
 
