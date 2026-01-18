0b41d2a51f82bf6f105f489360aef114e0c78c36
BLUE <isyouyoung@gmail.com>
2026-01-18 21:50:09 +0900

    공부

--- src/variable/ex10/ex01_Car/Ev.java
+++ src/variable/ex10/ex01_Car/Ev.java
@@ -2,13 +2,10 @@ package variable.ex10.ex01_Car;
 
 public class Ev extends Car{
 
-    // 필드 (필요없음)
-
-    // 생성자 (필요없음 => 자동생성)
-
-    // 메서드
     public void charge() {
         System.out.println("충전");
     }
+
+
 
 }
