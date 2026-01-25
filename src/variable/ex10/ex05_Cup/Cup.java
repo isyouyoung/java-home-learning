75950022bfed44af1a915d1827269867052e26d8
BLUE <isyouyoung@gmail.com>
2026-01-25 22:30:16 +0900

    공부

---
+++ src/variable/ex10/ex05_Cup/Cup.java
@@ -0,0 +1,18 @@
+package variable.ex10.ex05_Cup;
+
+public class Cup {
+  private Coffee coffee;
+  
+  public Cup() {
+    
+  }
+  public Coffee getCoffee() {
+    return coffee;
+  }
+  public void setCoffee(Coffee coffee) {
+    this.coffee = coffee;
+  }
+  public Cup(Coffee coffee) {
+    this.coffee = coffee;
+  }
+}
