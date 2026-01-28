ca3d820ac60c3620cb4649b82947439b2ad51a5f
BLUE <isyouyoung@gmail.com>
2026-01-28 22:19:20 +0900

    공부

---
+++ src/variable/ex10/ex10_Walkable/Animal.java
@@ -0,0 +1,16 @@
+package variable.ex10.ex10_Walkable;
+
+public class Animal {
+
+  private String name;
+
+  public Animal(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
+  }
+
+  }
