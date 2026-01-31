15f4dd4c209485c601410a87448222ff87cf7277
BLUE <isyouyoung@gmail.com>
2026-01-31 23:28:23 +0900

    공부

--- src/variable/ex10/ex01_Car/MainWrapper.java
+++ src/variable/ex10/ex01_Car/MainWrapper.java
@@ -3,18 +3,18 @@ package variable.ex10.ex01_Car;
 public class MainWrapper {
 
   public static void main(String[] args) {
-//    Ev ev = new Ev();
-//    ev.charge();
-//    ev.drive();
-//
-//    EngineCar enCar = new EngineCar();
-//    enCar.drive();
-//    enCar.oiling();
-//
-//    HybridCar hybrid = new HybridCar();
-//    hybrid.charge();
-//    hybrid.drive();
-//    hybrid.oiling();
+    Ev ev = new Ev();
+    ev.charge();
+    ev.drive();
+
+    EngineCar enCar = new EngineCar();
+    enCar.drive();
+    enCar.oiling();
+
+    HybridCar hybrid = new HybridCar();
+    hybrid.charge();
+    hybrid.drive();
+    hybrid.oiling();
 
   }
 
