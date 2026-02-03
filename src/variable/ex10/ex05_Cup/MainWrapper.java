8fd6ae6387f5b84a4c7773a763b85e77059a282e
BLUE <isyouyoung@gmail.com>
2026-02-03 21:46:48 +0900

    공부

---
+++ src/variable/ex10/ex05_Cup/MainWrapper.java
@@ -0,0 +1,15 @@
+package variable.ex10.ex05_Cup;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    Cup cup1 = new Cup();
+    Coffee c1 = new Americano();
+    cup1.setCoffee(c1);
+    
+    Cup cup2 = new Cup();
+    cup2.setCoffee(new CafeLatte());
+
+  }
+
+}
